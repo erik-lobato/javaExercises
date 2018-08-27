e3b181f686c53a65bf4123e7b6774a5c55a9698f
erik-lobato <42725459+erik-lobato@users.noreply.github.com>
2018-08-26 23:48:52 -0300

    Delete exer1.java

--- JavaBasicExercises/JavaBasicExercises/exer1.java
+++
@@ -1,8 +0,0 @@
-// Basic Exercises [ 128 Exercises with Solution ] 
-
-class exer1 {
-	public static void main(String[] args){
-		System.out.println("Hello");
-		System.out.println("Erik Lobato");
-	}
-}
