f28210a34b5002e2696c4fb1ea6831f2f5c2501f
erik-lobato <42725459+erik-lobato@users.noreply.github.com>
2018-08-26 22:47:55 -0300

    Add files via upload

---
+++ JavaBasicExercises/exer1.java
@@ -0,0 +1,8 @@
+// Basic Exercises [ 128 Exercises with Solution ] 
+
+class exer1 {
+	public static void main(String[] args){
+		System.out.println("Hello");
+		System.out.println("Erik Lobato");
+	}
+}
